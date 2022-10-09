6802ed7d1353d489ec72e6254a14fb6fc08eb1d9
MaycollJaramillo01 <maycolljaramillo01@gmail.com>
2022-10-08 19:57:57 -0600

    Js Changes

--- Js/Basic Javascript/Mayorque.js
+++ Js/Basic Javascript/Mayorque.js
@@ -30,3 +30,19 @@ function testGreaterOrEqual(val) {
   }
   
   testLessOrEqual(10);
+
+
+  function testLogicalAnd(val) {
+    // Cambia solo el código debajo de esta línea
+  
+    if (val >= 25 && val <= 50) {
+    
+        return "Yes";
+    
+    }
+  
+    // Cambia solo el código encima de esta línea
+    return "No";
+  }
+  
+  testLogicalAnd(10);
--- Js/Basic Javascript/if.js
+++ Js/Basic Javascript/if.js
@@ -9,3 +9,35 @@ function trueOrFalse(wasThatTrue) {
     // Cambia solo el código encima de esta línea
   
   }
+
+  function testElse(val) {
+    let result = "";
+    // Cambia solo el código debajo de esta línea
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }else{
+      result = "5 or Smaller";
+    }
+  
+    // Cambia solo el código encima de esta línea
+    return result;
+  }
+  
+  testElse(4);
+
+
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+  
+   else if (val < 5) {
+      return "Smaller than 5";
+    }
+  else{
+    return "Between 5 and 10";
+  }
+  }
+  
+  testElseIf(7);
